887094f0c3f049e6c7b8eeb42daa4298109912aa
Zachariah Cabelly <93895804+td5038@users.noreply.github.com>
2023-10-19 10:39:35 -0700

    Add new things before cloning for the first time

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" }
+  { import = "astrocommunity.pack.python_ruff" }
+  { import = "astrocommunity.pack.cpp" }
+  { import = "astrocommunity.pack.scala" }
 }
